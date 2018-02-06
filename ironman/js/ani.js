32e194d817134488635a095b9569b80c9f71928a
ireck22 <dragon9029@gmail.com>
2018-02-06 11:26:56 +0800

    update

--- ironman/js/ani.js
+++ ironman/js/ani.js
@@ -1,19 +1,14 @@
 
-  function ani(){
-  　$(window).load(function(){
-  　　$(window).bind('scroll resize', function(){
-  　　var $this = $(this);
-  　　var $this_Top=$this.scrollTop();
 
-  　　//當高度小於100時，關閉區塊
-  　　if($this_Top < 100){
-  　　　$('#tc1').stop().animate({top:"0px"});
-  　　　}
-  　　　　if($this_Top > 100){
+function ani (){
+      //  $("#response_menu").click(function(){
+        //    $(this).toggleClass("hamburger-x");
+        //});
+        $(document).ready(function() {
+          $('#response_menu').on('click', function(e){
+            e.preventDefault();
+            $('body').toggleClass('hamburger-x');
+          });
+        });
 
-  　　　　$('#tc1').animate({top:"0px"});
-          //$('#tc1').fadeIn();
-        }
-  　　}).scroll();
-  　});
-  }
+    }
